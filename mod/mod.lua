4371ecb0b39205cc95b3ff647d7bcd2b06f6b689
Niclas Schirrmeister <nschirrmeister@web.de>
2020-03-22 11:54:37 +0100

    project initialisation

---
+++ mod/mod.lua
@@ -0,0 +1,19 @@
+function data()
+    return {
+        info = {
+            minorVersion = 0,
+            severityAdd = "NONE",
+            severityRemove = "NONE",
+            name = _("Modular Train Station"),
+            description = _("modular_tram_station_desc"),
+            tags = { "Train Station" },
+            authors = {
+                {
+                    name = "EISFEUER",
+                    role = 'CREATOR',
+                },
+            },
+        },
+    }
+    end
+    
---
+++ mod/strings.lua
@@ -0,0 +1,11 @@
+function data()
+    return {
+      en = {
+        ["modular_tram_station_name"] = "Tram Station",
+        ["modular_tram_station_desc"] = "Modular station for trams and bus rapid transit.",
+      },
+      de = {
+      },
+    }
+end
+    
